10afe87ef06568518bdd5e4ed044ac2b6afae240
Giovanni <giovdigua@gmail.com>
2024-12-08 18:51:25 +0100

    Creating a ngFor loop to show multiple books

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { Book } from '../book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
 
-  book: Book = { id: 1, title: "Book one", author: "Author one" };
+  books: Book[] =[
+    { id: 1, title: "Book one", author: "Author one" }, 
+    { id: 2, title: "Book two", author: "Author two" }, 
+    { id: 3, title: "Book three", author: "Author three" }, 
+  ];
 }
