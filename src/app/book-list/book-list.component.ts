8dea9e98850a7ac1d927284059e6eb6c11fcecc9
Giovanni <giovdigua@gmail.com>
2024-12-08 18:41:57 +0100

    Binding a book object

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-list',
@@ -9,5 +10,5 @@ import { Component } from '@angular/core';
 })
 export class BookListComponent {
 
-  bookTitle: string = "My Book";
+  book: Book = { id: 1, title: "Book one", author: "Author one" };
 }
