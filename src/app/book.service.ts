d4c3c680577c9c72b27caafb1e2a968ae3d2a546
Giovanni <giovdigua@gmail.com>
2024-12-08 19:05:46 +0100

    Building the book service

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  private apiUrl = 'https://localhost:7174';
+  constructor(private http : HttpClient) { }
 }
